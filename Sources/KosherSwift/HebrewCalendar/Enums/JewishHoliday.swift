928a5544c9a9dfab488b78f2556dd4654104f98d
Daniel Smith <ds1239@hotmail.com>
2025-09-28 15:03:06 -0400

    fix

--- Sources/KosherSwift/HebrewCalendar/Enums/JewishHoliday.swift
+++ Sources/KosherSwift/HebrewCalendar/Enums/JewishHoliday.swift
@@ -15,7 +15,7 @@ public enum JewishHoliday: Int, CaseIterable {
     case erevPesach, pesach, cholHamoedPesach, pesachSheni, erevShavuos, shavuos, seventeenthOfTammuz, tishaBeav, tuBeav, erevRoshHashana, roshHashana, fastOfGedalia, erevYomKippur, yomKippur, erevSuccos, succos, cholHamoedSuccos, hoshanaRabba, sheminiAtzeres, simchasTorah, erevChanukah, chanukah, tenthOfTeves, tuBeshvat, fastOfEsther, purim, shushanPurim, purimKatan, erevRoshChodesh, roshChodesh, yomHashoah, yomHazikaron, yomHaatzmaut, yomYerushalaim, lagBaomer, shushanPurimKatan, isruChag
     
     /// If a erev chag is passed in, return true if this is a non-erev version and vis versa.
-    func isErevConnection(_ other: JewishHoliday) -> Bool {
+    public func isErevConnection(_ other: JewishHoliday) -> Bool {
         for connection in Self.connections {
             if connection.contains(self), connection.contains(other) {
                 return true
