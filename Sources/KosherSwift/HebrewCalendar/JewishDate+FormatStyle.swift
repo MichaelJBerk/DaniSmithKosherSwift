c3d80393003c9583a6bd2d0e57179067cb2a9705
Michael Berk <michaelberkdev@gmail.com>
2026-03-12 14:39:39 -0400

    Add option to pass value in JewishDate format style for hebrew and localize

--- Sources/KosherSwift/HebrewCalendar/JewishDate+FormatStyle.swift
+++ Sources/KosherSwift/HebrewCalendar/JewishDate+FormatStyle.swift
@@ -49,18 +49,18 @@ public extension JewishDate {
 		}
 
 		///Modifies the Jewish date format style to return the date in Hebrew
-		public func hebrew() -> Self {
+		public func hebrew(_ value: Bool = true) -> Self {
 			var newFS = self
-			newFS.inHebrew = true
+			newFS.inHebrew = value
 			return newFS
 		}
 
 		///Modifies the Jewish date format style to localize the output. 
 		/// 
 		///For example, the 23rd of Adar in 5786 would return "כ״ג באדר תשפ״ו" if set to `true`, and "כ״ג אדר תשפ״ו" if set to `false`. 
-		public func localize() -> Self {
+		public func localize(_ value: Bool = true) -> Self {
 			var newFS = self
-			newFS.shouldLocalize = true
+			newFS.shouldLocalize = value
 			return newFS
 		}
 
